996e57a2f10a498a616dcb945aa60bd2331b531f
TyroneHD <34520673+TyroneHD@users.noreply.github.com>
2018-01-20 22:44:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,7 +198,7 @@ client.on('message', (message) => {
             .then(function(id){
             roblox.getRankInGroup(groupId, id)
                 .then(function(rank){
-                    message.channel.send(`${message.author} | ${username} (https://www.roblox.com/users/${id}/profile)`)
+                    message.channel.send(`${message.author} | **${username}** (https://www.roblox.com/users/${id}/profile)`)
                 })
             })
         } else {
