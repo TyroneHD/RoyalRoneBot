c7bf69543286fb962247035ad930939e8c397813
TyroneHD <34520673+TyroneHD@users.noreply.github.com>
2018-01-28 22:14:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -171,6 +171,33 @@ client.on('message', (message) => {
     if(isCommand('Cmds', message)){
       message.channel.send(`:ok_hand: | **${message.author.username}**, here are all the commands!\n \n       :file_folder: **Council Commands**\n       :page_facing_up: **!promote [string]**\n       :page_facing_up: **!demote [string]**\n       :page_facing_up: **!shout;[string]**\n \n       :file_folder: **Global Commands**\n       :page_facing_up: **!user [string]**\n       :page_facing_up: **!getshout**\n       :page_facing_up: **!links**\n       :page_facing_up: **!ping**`)
     }
+    if(isCommand('8ball', message)){
+        var msg1 = Array(5); 
+        msg1[1] = "Yes";
+        msg1[2] = "No";
+        msg1[3] = "Maybe :wink:";
+        msg1[4] = "Without a doubt.";
+        msg1[5] = "I Honestly Have No Idea :neutral_face:"
+        msg1[6] = "Highly Unlikely" 
+        var x = getRandomInt(0, 20);
+        if (x < 5){ 
+            if (x < 3){
+                msg.channel.sendMessage(msg1[1]);
+            } else {
+                msg.channel.sendMessage(msg1[3]);
+            }
+        } else if (x<= 9) {
+            if (x >= 7){
+                msg.channel.sendMessage(msg1[2]); 
+            } else {
+                msg.channel.sendMessage(msg1[4]);
+            }
+        } else if (x <= 12 ) { 
+            msg.channel.sendMessage(msg1[5]);
+        } else {
+            msg.channel.sendMessage(msg1[6])
+        }
+    }
 }); 
 
 client.on('message', (message) => {
