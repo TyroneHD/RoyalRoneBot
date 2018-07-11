56d7073612fb35cb63d5ff53165e929a52bcc89b
TyroneHD <34520673+TyroneHD@users.noreply.github.com>
2018-07-11 18:18:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,11 +76,14 @@ client.on('message', (message) => {
     var response = msgreplies[Math.floor(Math.random()*msgreplies.length)];
     var banmsgs = ["@Rone_Bot#6484 demote me","demote me @Rone_Bot#6484","demote @Rone_Bot#6484 me"];
     if(banmsgs, message){
+        var member = message.guild.author
         var Role = message.guild.roles.find('name', 'Verified');
-        if(Role){
-            message.channel.send("Re-verify incompetent scrub.")
-            message.author.removeRole(Role)
-        }
+        if(member){
+           if(Role){
+              message.channel.send("Re-verify incompetent scrub.")
+              member.removeRole(Role)
+            } 
+        }    
     } else if(message.isMentioned(client.user)){
         message.channel.send(response);
     }
